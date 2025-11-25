d527d8ddd4f789bb886c1f198ba724aeb427e7be
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2025-11-25 00:10:00 +0000

    added changes to root main.tf, added alb module and referenced another variable in the ecs module

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,10 +8,15 @@ module "ecs" {
   source     = "./modules/ecs"
   subnet_id1 = module.vpc.subnet1_id
   subnet_id2 = module.vpc.subnet2_id
+  target_group_arn = module.alb.target_group_arn
 }
 
 module "alb" {
   source = "./modules/alb"
   subnet_id1 = module.vpc.subnet1_id
   subnet_id2 = module.vpc.subnet2_id
+  region = var.dog
+  vpc_id = module.vpc.vpc_id
+  vpc_cidr = var.cat
 }
+
