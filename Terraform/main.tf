5b767c5f10a885dd4b3f8261f39ade2569df9a7e
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2025-11-29 11:52:42 +0000

    Modified main.tf, added route53 module

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,18 +5,29 @@ module "vpc" {
 }
 
 module "ecs" {
-  source     = "./modules/ecs"
-  subnet_id1 = module.vpc.subnet1_id
-  subnet_id2 = module.vpc.subnet2_id
-  target_group_arn = module.alb.target_group_arn
+  source            = "./modules/ecs"
+  subnet_id1        = module.vpc.subnet1_id
+  subnet_id2        = module.vpc.subnet2_id
+  target_group_arn  = module.alb.target_group_arn
+  ecs_sg            = module.alb.ecs_sg
+  load_balancer_arn = module.alb.load_balancer_arn
 }
 
 module "alb" {
-  source = "./modules/alb"
+  source     = "./modules/alb"
   subnet_id1 = module.vpc.subnet1_id
   subnet_id2 = module.vpc.subnet2_id
-  region = var.dog
-  vpc_id = module.vpc.vpc_id
-  vpc_cidr = var.cat
+  region     = var.dog
+  vpc_id     = module.vpc.vpc_id
+  vpc_cidr   = var.cat
+  cert_arn = module.route53.cert_arn
 }
+
+module "route53" {
+  source      = "./modules/route53"
+  alb_dns     = module.alb.alb_dns
+  alb_zone_id = module.alb.alb_zone_id
+}
+
+
 
